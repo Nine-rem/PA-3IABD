ce822ec34b9fe8b2df4c9339a142f8077ccee538
rpalmier <rpalmier@et.intechinfo.fr>
2025-04-29 18:18:15 +0200

    linearModel

--- src/lib/linear_model.rs
+++ src/lib/linear_model.rs
@@ -1,17 +1,66 @@
 pub struct LinearModel {
-    weights:
-    bias: 
+    weights: Vec<f64>,
+    bias: f64,
+}
+
+fn simple_random(seed: u64, index: usize) -> f64 {
+    let prime = 31u64;
+    let mut value = seed.wrapping_mul((index as u64 + 1) * prime);
+    value = value.wrapping_add(12345);
+    (value % 1000) as f64 / 1000.0
 }
 
 impl LinearModel {
-    pub fn new(){}
+    pub fn new(dim: usize)-> Self{ 
+        let mut weights =  Vec::new();
+        for i in 0..dim {
+            weights.push(simple_random(42, i));
+        }
+        let bias = simple_random(42, dim + 1000);
+        LinearModel {
+            weights,
+            bias,
+        }
+    }
     
-    pub fn predict(){}
+    pub fn predict(&self, inputs: &[f64]) -> f64 {
+        let mut somme = 0.0;
+        for (w, x) in self.weights.iter().zip(inputs.iter()) {
+            somme += *w * *x;
+        }
+        somme += self.bias;
+        return somme;
+    }
 
-    pub fn fit(){}
+    pub fn fit(&mut self, X:&Vec<Vec<f64>>,y: &Vec<f64>, learning_rate: f64, epochs: usize){
+        
+        let predicted_length = X.len();
 
-    pub fn get_weights(){}
+        for epoch in 0..epochs {
+           let mut total_loss = 0.0;
+           for i in 0..predicted_length {
+                let prediction = self.predict(&X[i]);
+                let error = y[i] - prediction;
+                for j in 0..self.weights.len() {
+                    self.weights[j] += learning_rate * error * X[i][j];
+                }
+                self.bias += learning_rate * error;
+                total_loss += error.powi(2); //convergence                
+           } 
+            // Afficher la perte à chaque époque 
+            if epoch % 50 == 0 {
+                let loss = total_loss / predicted_length as f64;
+                println!("Epoch: {}, Loss: {}", epoch + 1, loss);
+            }
+        }
+    }
 
-    pub fn get_bias(){}
+    pub fn get_weights(&self) -> &Vec<f64> {
+        &self.weights
+    }
+
+    pub fn get_bias(&self) -> f64 {
+        self.bias
+    }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+mod lib;
+use lib::linear_model::LinearModel;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Start");
+
+    let mut model = LinearModel::new(3);
+
+    let X = vec![
+        vec![1.0, 2.0, 3.0],
+        vec![2.0, 3.0, 4.0],
+        vec![3.0, 4.0, 5.0],
+    ];
+
+    let y = vec![ 7.0, 9.0, 10.0];
+
+    model.fit(&X, &y, 0.001, 10000);
+
+    println!("Poids après l'entraînement : {:?}", model.get_weights());
+    println!("Biais après l'entraînement : {}", model.get_bias());
 }
+
+
